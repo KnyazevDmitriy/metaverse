7332d27befbb99cc0e7e83cedb8166faf43aec40
Дмитрий Князев <105502040+KnyazevDmitriy@users.noreply.github.com>
2022-07-12 01:08:09 +0300

    modals

--- src/js/modal.js
+++ src/js/modal.js
@@ -13,8 +13,25 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
+var modall = document.getElementById("myModal-2");
+
+var button = document.getElementById("myBtn2");
+
+var span = document.getElementsByClassName("close2")[0];
+
+button.onclick = function() {
+    modall.style.display = "block";
+}
+
+span.onclick = function() {
+    modall.style.display = "none";
+}
+
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+    if (event.target == modall) {
+            modall.style.display = "none";
     }
 }
