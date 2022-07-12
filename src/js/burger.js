d0925f8cdc3413388d7de4785b5aced013abcc53
Дмитрий Князев <105502040+KnyazevDmitriy@users.noreply.github.com>
2022-07-12 08:38:23 +0300

    burger

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,12 +1,15 @@
 const burger = document.querySelector('.burger')
 const navMenu = document.querySelector('.nav-menu')
+const body = document.querySelector('body')
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active')
     navMenu.classList.toggle('active')
+    body.classList.toggle('active')
 })
 
-document.querySelectorAll('.link').forEach(n => n.addEventListener('click', () => {
+document.querySelectorAll('.link, .nav_sign-up, .nav_sign-in').forEach(n => n.addEventListener('click', () => {
     burger.classList.remove('active')
     navMenu.classList.remove('active')
+    body.classList.remove('active')
 }))
